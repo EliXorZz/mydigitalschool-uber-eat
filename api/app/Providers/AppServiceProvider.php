7aa9300da903c513a8f459506baa055142d0397b
EliXorZz <dylan.battig@gmail.com>
2026-06-04 12:17:33 +0200

    chore: increase rate limit

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
             });
 
         RateLimiter::for('auth', function (Request $request) {
-            return Limit::perMinute(5)->by($request->ip());
+            return Limit::perMinute(30)->by($request->ip());
         });
 
         RateLimiter::for('api', function (Request $request) {
